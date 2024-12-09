d1b44cf494d279ea0d90693363cb4b7b6cf00507
Benjamin Rice <benjamin.rice.20@cnu.edu>
2024-12-08 21:05:53 -0500

    second commit

--- target/debug/deps/seize-45f433cd280066e0.d
+++ target/debug/deps/seize-45f433cd280066e0.d
@@ -1,21 +1,15 @@
-C:\Users\HP\IdeaProjects\MPPProject\seize\target\debug\deps\libseize-45f433cd280066e0.rmeta: src\lib.rs src\collector.rs src\deferred.rs src\guard.rs src\raw.rs src\tls\mod.rs src\tls\thread_id.rs src\utils.rs src\guide.rs src\reclaim.rs src\structures\mod.rs src\structures\lock_free_queue.rs src\structures\atomic_queue.rs src\../README.md src\../docs/GUIDE.md
+C:\Users\HP\IdeaProjects\MPPProject\Pranav\seize\target\debug\deps\libseize-45f433cd280066e0.rmeta: src\lib.rs src\collector.rs src\guard.rs src\raw.rs src\tls\mod.rs src\structures\mod.rs src\structures\lock_free_queue.rs src\structures\atomic_queue.rs src\../README.md
 
-C:\Users\HP\IdeaProjects\MPPProject\seize\target\debug\deps\libseize-45f433cd280066e0.rlib: src\lib.rs src\collector.rs src\deferred.rs src\guard.rs src\raw.rs src\tls\mod.rs src\tls\thread_id.rs src\utils.rs src\guide.rs src\reclaim.rs src\structures\mod.rs src\structures\lock_free_queue.rs src\structures\atomic_queue.rs src\../README.md src\../docs/GUIDE.md
+C:\Users\HP\IdeaProjects\MPPProject\Pranav\seize\target\debug\deps\libseize-45f433cd280066e0.rlib: src\lib.rs src\collector.rs src\guard.rs src\raw.rs src\tls\mod.rs src\structures\mod.rs src\structures\lock_free_queue.rs src\structures\atomic_queue.rs src\../README.md
 
-C:\Users\HP\IdeaProjects\MPPProject\seize\target\debug\deps\seize-45f433cd280066e0.d: src\lib.rs src\collector.rs src\deferred.rs src\guard.rs src\raw.rs src\tls\mod.rs src\tls\thread_id.rs src\utils.rs src\guide.rs src\reclaim.rs src\structures\mod.rs src\structures\lock_free_queue.rs src\structures\atomic_queue.rs src\../README.md src\../docs/GUIDE.md
+C:\Users\HP\IdeaProjects\MPPProject\Pranav\seize\target\debug\deps\seize-45f433cd280066e0.d: src\lib.rs src\collector.rs src\guard.rs src\raw.rs src\tls\mod.rs src\structures\mod.rs src\structures\lock_free_queue.rs src\structures\atomic_queue.rs src\../README.md
 
 src\lib.rs:
 src\collector.rs:
-src\deferred.rs:
 src\guard.rs:
 src\raw.rs:
 src\tls\mod.rs:
-src\tls\thread_id.rs:
-src\utils.rs:
-src\guide.rs:
-src\reclaim.rs:
 src\structures\mod.rs:
 src\structures\lock_free_queue.rs:
 src\structures\atomic_queue.rs:
 src\../README.md:
-src\../docs/GUIDE.md:
